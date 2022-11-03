808db6ec5a1d439701793680b0e5725b9b631f37
farhatdk <farhatdk98@gmail.com>
2022-11-03 20:23:09 +0700

    style adjust

--- src/components/DashboardLayout/FooterMobile/index.tsx
+++ src/components/DashboardLayout/FooterMobile/index.tsx
@@ -1,51 +1,22 @@
-import s from './FooterMobile.module.scss'
-import GlobeBlueSvg from './../../../../public/assets/svg/globe-blue.svg';
-import Image from 'next/image';
-import Link from 'next/link'
+// import s from "./FooterMobile.module.scss";
+import playstoreDownload from "./../../../../public/footer-hrd.png";
+import styles from "./../../DashboardLayout/DashboardLayout.module.scss";
+import GlobeBlueSvg from "./../../../../public/assets/svg/globe-blue.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function FooterMobile() {
-    return (
-        <>
-            <div className={s.container}>
-                <Link href="/global">
-                    <a className={`${s.menuWrapper} ${s.active}`}>
-                        <div className={`${s.logoWrapper} ${s.globe}`}>
-                        </div>
-                        <div className={s.title}>
-                            Menu 1
-                        </div>
-                    </a>
-                </Link>
-                <Link href="/halaman">
-                    <a className={`${s.menuWrapper}`}>
-                        <div className={`${s.logoWrapper} ${s.globe}`}>
-                        </div>
-                        <div className={s.title}>
-                            Menu 2
-                        </div>
-                    </a>
-                </Link>
-
-                <Link href="/pengumuman">
-                    <a className={`${s.menuWrapper}`}>
-                        <div className={`${s.logoWrapper} ${s.globe}`}>
-                        </div>
-                        <div className={s.title}>
-                            Menu 3
-                        </div>
-                    </a>
-                </Link>
-
-                <Link href="/dashboard/komentar">
-                    <a className={`${s.menuWrapper}`}>
-                        <div className={`${s.logoWrapper} ${s.globe}`}>
-                        </div>
-                        <div className={s.title}>
-                            Keluar
-                        </div>
-                    </a>
-                </Link>
-            </div>
-        </>
-    )
+  return (
+    <div className={styles.iconFooter}>
+      <Link
+        href="https://play.google.com/store/apps/details?id=com.koneksi.akhlaqchat"
+        passHref
+      >
+        <a>
+          <Image width={80} height={24} src={playstoreDownload} />
+          <span>Download Sekarang Juga!</span>
+        </a>
+      </Link>
+    </div>
+  );
 }
